55d3fd3c8c10956374f81ad5fa98f8a5fb892d33
Allan Benedict <allan.benedict@kratin.co.in>
2025-03-04 17:37:29 +0530

    added comments to prgram.cs

--- Program.cs
+++ Program.cs
@@ -1,25 +1,29 @@
-var builder = WebApplication.CreateBuilder(args);
-
+var builder = WebApplication.CreateBuilder(args); // initialize an instance of webapp builder class,
+//sets up configuration,servcies of the webserver.
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(); // mvc dependancy injection container with support for controllers.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(); // adding swagger
 
-var app = builder.Build();
+var app = builder.Build(); // creates a webapp instance 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())//This checks if the application is running in the Development environment.
+
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    
+    app.UseSwagger(); //This sets up the Swagger middleware to generate API documentation.
+    app.UseSwaggerUI();//This enables the Swagger user interface (UI) where you can test API endpoints.
 }
 
-app.UseHttpsRedirection();
+app.UseHttpsRedirection(); //redirects http to https
 
-app.UseAuthorization();
+app.UseAuthorization(); // enables authorization middleware, [Authorize] is used
+//authorized users are allowed to access that controller action.
 
-app.MapControllers();
+app.MapControllers(); // enables routing API controllers, those using [ApiController]
+//without this, controller endpoints won't work.
 
-app.Run();
+app.Run();// starts the program application
